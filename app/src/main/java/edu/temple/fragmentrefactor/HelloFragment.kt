db2907f633776d6da282e65fda2707ab0cd9e289
Timothy <timkhumpan@gmail.com>
2024-10-14 15:22:23 -0400

    add textview button and evitview to fragment

--- app/src/main/java/edu/temple/fragmentrefactor/HelloFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/HelloFragment.kt
@@ -1,5 +1,6 @@
 package edu.temple.fragmentrefactor
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +14,15 @@ class HelloFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hello, container, false)
+        return inflater.inflate(R.layout.fragment_hello, container, false).apply {
+//            setOnClickListener {
+//                setBackgroundColor(
+//                    Color.parseColor(
+//                        arrayOf("blue", "cyan", "green").random()
+//                    )
+//                )
+//            }
+
+        }
     }
 }
